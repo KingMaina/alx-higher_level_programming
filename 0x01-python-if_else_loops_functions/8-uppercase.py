6c0e120b50091e1011ffb77eccda9af9ca7b4baa
Andrew <drew29799@gmail.com>
2023-08-10 14:57:56 -0700

    Add: Python functions
    * Added function to remove a character a given index.
    * Defined a function from bytecode
    * Fixed typos and logic in other files

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for letter in str:
-        print("{}".format(chr(ord(letter)) - 32), end="")
-    print("".format(), end="")
+        if "a" <= letter <= "z":
+            letter = chr(ord(letter) - ord("a") + ord("A"))
+        print("{}".format(letter), end="")
+    print()
