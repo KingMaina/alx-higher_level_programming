a6b302f06f862466eeb8f02e1abfcdc374395e6a
Andrew <drew29799@gmail.com>
2023-08-11 15:09:22 -0700

    Fix: calculator function

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,25 +2,32 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
+
 def calculate(num1, num2, operator):
-    return int(num1) operator int(num2)
+    if operator == "+":
+        return add(int(num1), int(num2))
+    elif operator == "-":
+        return sub(int(num1), int(num2))
+    elif operator == "*":
+        return mul(int(num1), int(num2))
+    elif operator == "/":
+        return div(int(num1), int(num2))
+
 
 def main():
     operators = ["+", "-", "*", "/"]
     args = sys.argv.copy()[1:]
     args_len = len(args)
+    if args_len != 3:
+        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
+        sys.exit(1)
     operator = args[1]
     num1 = args[0]
     num2 = args[2]
-    if args_len != 3:
-        print(f"Usage: {sys.args[0]} <a> <operator> <b>")
-        sys.exit(1)
     if operator not in operators:
         print(f"Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    match operator:
-        case "+":
-            result = calculate(num1, num2, operator)
+    result = calculate(num1, num2, operator)
     print(f"{num1} {operator} {num2} = {result}")
 
 
