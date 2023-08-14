04542352da07be3f1f8d299e094a1d18fc1df60d
Andrew <drew29799@gmail.com>
2023-08-14 14:47:06 -0700

    Fix: logic

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for item in my_list:
-        print("{}".format(item))
+    my_list_len = len(my_list)
+    if my_list_len > 0:
+        for item in my_list:
+            print("{}".format(item))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     my_list_len = len(my_list)
     my_list_copy = my_list.copy()
-    if my_list_len <= 0 or idx >= my_list_len:
+    if my_list_len < 0 or idx >= my_list_len:
         return my_list_copy
     my_list_copy[idx] = element
     return my_list_copy
