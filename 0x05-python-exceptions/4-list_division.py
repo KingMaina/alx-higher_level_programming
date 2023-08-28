409ac2ad9263db0736f386c14d7c7bb5936b67ff
Andrew <drew29799@gmail.com>
2023-08-29 00:03:03 +0300

    Added python functions

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,25 @@
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError("out of range")
+            num1 = my_list_1[i]
+            num2 = my_list_2[i]
+            if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)):
+                print("wrong type")
+                result.append(0)
+            elif num2 == 0:
+                print("division by 0")
+                result.append(0)
+            else:
+                result.append(num1 / num2)
+        except IndexError as e:
+            print(e)
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        finally:
+            pass
+    return result
