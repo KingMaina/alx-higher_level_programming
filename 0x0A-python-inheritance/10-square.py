865cb0bedb4813074c7759144d94a22d014353c2
Andrew <drew29799@gmail.com>
2023-09-12 09:47:48 +0300

    Fix: Module documentation location

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-Rectangle = __import__('9-rectangle').Rectangle
-
 """Module containing a Rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 """Module containing a Rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
