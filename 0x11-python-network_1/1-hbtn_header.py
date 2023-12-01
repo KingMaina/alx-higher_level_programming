76c69a2b4c6549f88272ec32b11a66d1feda0000
Andrew <drew29799@gmail.com>
2023-12-01 20:09:47 +0300

    Add: Script that requests and displays a header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request and displays the value
+of the X-Request-Id in the response header
+"""
+from sys import argv
+import urllib.request
+
+url = argv[1]
+request = urllib.request.Request(url)
+with urllib.request.urlopen(request) as response:
+    headers = response.headers
+    x_request_id = headers.get('X-Request-Id')
+    print(x_request_id)
